7701b2eee3d2471bfca62430b0cc6ab8cfdb63c1
JuntOta <junto.ota@ezweb.ne.jp>
2021-04-26 15:33:38 +0900

    新規登録のバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}, presence: true
   validates :nickname, presence: true
   validates :family_name, presence: true
   validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :family_name_kana, format: { with: /\A([ァ-ン]|ー)+\z/}, presence: true
+  validates :first_name_kana, format: { with: /\A([ァ-ン]|ー)+\z/}, presence: true
   validates :birthday, presence: true
 
   has_many :items
