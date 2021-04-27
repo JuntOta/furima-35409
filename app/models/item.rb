40d1531d04c569f681775268517f6cf6b1be78ce
JuntOta <junto.ota@ezweb.ne.jp>
2021-04-27 20:52:30 +0900

    バリデーション設定2回目

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,9 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :introduction
-    validates :price
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999},
+    format: { with: /\A[0-9]+\z/}
+    validates :image
   end
   with_options numericality: { other_than: 1 } do
     validates :category_id
