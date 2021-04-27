2348702406ac5bb88ec09c14adbaff8db6ea7d40
JuntOta <junto.ota@ezweb.ne.jp>
2021-04-27 14:43:03 +0900

    ActiveHashの導入、バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,14 +3,24 @@ class Item < ApplicationRecord
     validates :name
     validates :introduction
     validates :price
+  end
+  with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :item_condition_id
     validates :postage_type_id
-    validates :prefecture_id
     validates :preparation_day_id
   end
+
+  validates :prefecture_id, numericality: true
 
   belongs_to :user
   has_one :purchase_history
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_condition
+  belongs_to :postage_type
+  belongs_to :prefecture_id
+  belongs_to :preparation_day
 end
