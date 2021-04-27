c0bac5958351cc0f2dcda84e5e4911495fdeaf14
JuntOta <junto.ota@ezweb.ne.jp>
2021-04-27 11:59:40 +0900

    モデル、テーブル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,17 @@ class ItemsController < ApplicationController
   def index
   end
 
-  
+  def new
+    @items = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :category_id, :item_condition_id, :postage_type_id,
+    :prefecture_id, :preparation_day_id)
+  end
 end
---
+++ app/models/item.rb
@@ -0,0 +1,15 @@
+class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :introduction
+    validates :price
+    validates :category_id
+    validates :item_condition_id
+    validates :postage_type_id
+    validates :prefecture_id
+    validates :preparation_day_id
+  end
+
+  belongs_to :user
+  has_one :purchase_history
+end
