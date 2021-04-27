2dc36c7e46338a2bead8ee6100670352f365af14
JuntOta <junto.ota@ezweb.ne.jp>
2021-04-27 12:12:13 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,7 +13,7 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :introduction, :price, :category_id, :item_condition_id, :postage_type_id,
-    :prefecture_id, :preparation_day_id)
+    params.require(:item, :image).permit(:name, :introduction, :price, :category_id, :item_condition_id, :postage_type_id,
+    :prefecture_id, :preparation_day_id).merge(user_id: current_user.id)
   end
 end
